c48c3835602da3c9e313d80457f65c6244cefd55
David Nevado <david@davidnevado.xyz>
2023-04-08 18:46:22 +0200

    Export CurveEndo

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,7 @@ pub mod serde;
 #[macro_use]
 mod derive;
 pub use arithmetic::CurveAffineExt;
+pub use arithmetic::CurveEndo;
 pub use pasta_curves::arithmetic::{Coordinates, CurveAffine, CurveExt};
 
 // Re-export ff and group to simplify down stream dependencies
